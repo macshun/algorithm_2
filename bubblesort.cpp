54c8dd73bc04fb32bbcd3b39e21be6ce9e36832d
mcs <changshunma@gmail.com>
2018-09-09 22:03:03 +0800

    Create 1

---
+++ bubblesort.cpp
@@ -0,0 +1,28 @@
+/*
+	Name:  ц╟ещеепР 
+	Copyright: 
+	Author: mac
+	Date: 10/09/18 10:29
+	Description: 
+*/
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main()
+{
+	int num[5] = {3,4,2,1,5};
+	for(int i = 0;i < 5; i++){
+		for(int j = 1;j < 5 - i; j++){		
+			if(num[j-1] > num[j]){
+				int temp = num[j-1];
+				num[j-1] = num[j];
+				num[j] = temp;
+			}
+		}
+	}
+	for(int i = 0;i < 5; i++){
+		printf("%d ",num[i]);
+	}
+	return 0;
+}
+
