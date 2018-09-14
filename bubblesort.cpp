055aa8574bf3f3bdc39f6900c2a262d93eb77589
macshun <36837727+macshun@users.noreply.github.com>
2018-09-14 10:06:12 +0800

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,5 +1,5 @@
 /*
-	Name:  √∞≈›≈≈–Ú 
+	Name:  √É¬∞√Ö√ù√Ö√Ö√ê√≤ 
 	Copyright: 
 	Author: mac
 	Date: 10/09/18 10:29
@@ -11,8 +11,8 @@ using namespace std;
 int main()
 {
 	int num[5] = {3,4,2,1,5};
-	for(int i = 0;i < 5; i++){
-		for(int j = 1;j < 5 - i; j++){		
+	for(int i = 1;i < 5; i++){
+		for(int j = 0;j < 5 - i; j++){		
 			if(num[j-1] > num[j]){
 				int temp = num[j-1];
 				num[j-1] = num[j];
